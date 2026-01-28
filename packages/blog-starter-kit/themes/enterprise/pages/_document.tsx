6ad62a785bbc9b7da5b6d84dbac2b2caf922d5c5
Stephen J. Lu <Stephen@StephenJLu.com>
2026-01-28 12:27:03 -0700

    dark mode dev

--- packages/blog-starter-kit/themes/enterprise/pages/_document.tsx
+++ packages/blog-starter-kit/themes/enterprise/pages/_document.tsx
@@ -1,9 +1,21 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
+	const darkModeScript = `
+    (function() {
+      const saved = localStorage.getItem('darkMode');
+      const isDark = saved ? JSON.parse(saved) : window.matchMedia('(prefers-color-scheme: dark)').matches;
+      if (isDark) {
+        document.documentElement.classList.add('dark');
+      }
+    })();
+  `;
+
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<script dangerouslySetInnerHTML={{ __html: darkModeScript }} />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
