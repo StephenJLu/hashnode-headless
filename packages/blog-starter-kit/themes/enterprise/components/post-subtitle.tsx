118b965c66477a2cbf2bcf6bcf747af37af0ea21
Stephen J. Lu <Stephen@StephenJLu.com>
2026-01-28 20:05:06 -0700

    subtitle dev post pages

--- packages/blog-starter-kit/themes/enterprise/components/post-subtitle.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/post-subtitle.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const PostSubtitle = ({ children }: Props) => {
 	return (
 		<div className="prose md:prose-lg dark:prose-invert prose-p:text-center mx-auto max-w-screen-lg px-5">
-			<p className="text-slate-600 dark:text-neutral-400">{children}</p>
+			<h3 className="text-slate-600 dark:text-neutral-400">{children}</h3>
 		</div>
 	);
 };
